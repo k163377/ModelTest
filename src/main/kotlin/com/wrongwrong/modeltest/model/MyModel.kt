78f9e806e8d0c3c226dbbb5440831c292ea8cfea
Unknown <boranti1995@gmail.com>
2018-12-02 15:01:15 +0900

    add model

---
+++ src/main/kotlin/com/wrongwrong/modeltest/model/MyModel.kt
@@ -0,0 +1,10 @@
+package com.wrongwrong.modeltest.model
+
+import java.util.*
+
+data class MyModel(
+        val id: Long,
+        val name: String,
+        val create: Date,
+        val update: Date
+)
