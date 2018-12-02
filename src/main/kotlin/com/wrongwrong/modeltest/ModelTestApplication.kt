5574007bd2621649c63e4e8d78bc4f5100f8d3f1
Unknown <boranti1995@gmail.com>
2018-12-02 13:59:42 +0900

    Initial commit

---
+++ src/main/kotlin/com/wrongwrong/modeltest/ModelTestApplication.kt
@@ -0,0 +1,11 @@
+package com.wrongwrong.modeltest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ModelTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<ModelTestApplication>(*args)
+}
