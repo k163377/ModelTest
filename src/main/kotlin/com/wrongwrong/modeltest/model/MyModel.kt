aec039116012923ef5bfa0c666508066ecc0771b
Unknown <boranti1995@gmail.com>
2018-12-03 10:51:10 +0900

    やっぱいらなかった

--- src/main/kotlin/com/wrongwrong/modeltest/model/MyModel.kt
+++ src/main/kotlin/com/wrongwrong/modeltest/model/MyModel.kt
@@ -2,13 +2,11 @@ package com.wrongwrong.modeltest.model
 
 import java.util.*
 import javax.validation.constraints.AssertTrue
-import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
 data class MyModel(
         @field:NotNull(message = "idはnull不許可")
         val id: Long?,
-        @field:NotBlank(message = "nameは空白不許可")
         val name: String?,
         val create: Date?,
         val update: Date?
