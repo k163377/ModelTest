f29c02c97b0f7ec080d607b9b439757cfe7048f0
Unknown <boranti1995@gmail.com>
2018-12-03 16:51:35 +0900

    add annotation

--- src/main/kotlin/com/wrongwrong/modeltest/model/MyModel.kt
+++ src/main/kotlin/com/wrongwrong/modeltest/model/MyModel.kt
@@ -1,5 +1,6 @@
 package com.wrongwrong.modeltest.model
 
+import com.wrongwrong.modeltest.annotation.CanSplitBySpace
 import java.util.*
 import javax.validation.constraints.AssertTrue
 import javax.validation.constraints.NotNull
@@ -7,6 +8,7 @@ import javax.validation.constraints.NotNull
 data class MyModel(
         @field:NotNull(message = "idはnull不許可")
         val id: Long?,
+        @CanSplitBySpace(message = "名前は半角スペースで2つに分割できる")
         val name: String?,
         val create: Date?,
         val update: Date?
