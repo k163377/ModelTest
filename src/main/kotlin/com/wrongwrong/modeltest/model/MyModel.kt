dd9e6e23b91a498c387498a8713e96af9eacfa42
Unknown <boranti1995@gmail.com>
2018-12-03 09:56:08 +0900

    fix validation

--- src/main/kotlin/com/wrongwrong/modeltest/model/MyModel.kt
+++ src/main/kotlin/com/wrongwrong/modeltest/model/MyModel.kt
@@ -2,11 +2,13 @@ package com.wrongwrong.modeltest.model
 
 import java.util.*
 import javax.validation.constraints.AssertTrue
+import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
 data class MyModel(
         @field:NotNull(message = "idはnull不許可")
         val id: Long?,
+        @field:NotBlank(message = "nameは空白不許可")
         val name: String?,
         val create: Date?,
         val update: Date?
