d3137e64a7a84e5ce645737016e58ee2f90f5a47
Unknown <boranti1995@gmail.com>
2018-12-04 17:14:27 +0900

    コメント追加

--- src/main/kotlin/com/wrongwrong/modeltest/annotation/IsLater.kt
+++ src/main/kotlin/com/wrongwrong/modeltest/annotation/IsLater.kt
@@ -30,14 +30,15 @@ class IsLaterValidator: ConstraintValidator<IsLater, Any> {
     }
 
     override fun isValid(value: Any?, context: ConstraintValidatorContext?): Boolean {
-        //
+        //valueがnullなら何もしない
         if(value == null) return true
-
         val beanWrapper = BeanWrapperImpl(value)
 
+        //入力から値を取り出す
         val before = beanWrapper.getPropertyValue(beforeName) as Date?
         val after = beanWrapper.getPropertyValue(afterName) as Date?
 
+        //比較結果を返却
         if(before == null || after == null) return true
         return before.before(after) || before == after
     }
